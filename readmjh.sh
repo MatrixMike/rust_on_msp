9c9eefc950a43a59a756f250e1973db5a342f253
Mike on Tecra <matroxmike@icloud.com>
2020-10-20 00:08:31 +1100

    rust-src

--- readmjh.sh
+++ readmjh.sh
@@ -3,6 +3,7 @@ curl https://sh.rustup.rs -sSf | sh
 rustup default nightly
 cargo
 cargo install xargo --force
+rustup component add rust-src
 # https://www.oreilly.com/library/view/rust-programming-by/9781788390637/e07dc768-de29-482e-804b-0274b4bef418.xhtml
 xargo
 
