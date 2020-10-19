573483f0a7caf9073de663b2cddd7ddeda29c3bd
Mike on Tecra <matroxmike@icloud.com>
2020-10-20 00:04:54 +1100

    improve script

--- readmjh.md
+++
@@ -1,6 +0,0 @@
-curl https://sh.rustup.rs -sSf | sh
-rust
-cargo
-cargo install xargo
-xargo
-
--- readmjh.sh
+++ readmjh.sh
@@ -2,6 +2,7 @@
 curl https://sh.rustup.rs -sSf | sh
 rustup default nightly
 cargo
-cargo install xargo
+cargo install xargo --force
+# https://www.oreilly.com/library/view/rust-programming-by/9781788390637/e07dc768-de29-482e-804b-0274b4bef418.xhtml
 xargo
 
